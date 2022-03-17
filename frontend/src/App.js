8d4ad7ad259187698d72ead4c498e0e201e6b5b5
Ben Harned <ben.harned@rakuten.com>
2022-03-18 00:02:10 +0900

    Add react bootstrap and set up header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { Container } from 'react-bootstrap'
 const App = () => {
   return (
     <>
       <Header/>
-      <main>
-        <h1>Welcome to Proshop!</h1>
-      </main>
+        <main className='py-3'>
+          <Container>
+            <h1>Welcome to Proshop!</h1>
+          </Container>
+        </main>
       <Footer/>
     </>
   );
