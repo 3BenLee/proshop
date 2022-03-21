489925e76541483d1a8affc8f5f5057a3d43368b
BenLee3 <bendesigns76@gmail.com>
2022-03-21 20:56:09 +0900

    Add and setup React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 const App = () => {
   return (
-    <>
-      <Header/>
-        <main className='py-3'>
-          <Container>
-            <HomeScreen/>
-          </Container>
-        </main>
-      <Footer/>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
