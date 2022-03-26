be763f8667d8ccfe2960305104f116aee5db8d40
BenLee3 <bendesigns76@gmail.com>
2022-03-27 01:20:50 +0900

    Add concurrently, nodemon, dotenv to project

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const products = require('./data/products');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, console.log('Server running on Port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+);
 
 app.get('/', (req, res) => {
-  res.send('API is running');
+  res.send('API is running...');
 });
 
 app.get('/api/products', (req, res) => {
