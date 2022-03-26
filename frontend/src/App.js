7210f95e803f8264fa41e105cd1cfba55857c4c5
BenLee3 <bendesigns76@gmail.com>
2022-03-26 16:07:15 +0900

    Setup express server, routes, useState, and useEffect for products and product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,28 @@
-import React from 'react';
-// temp js file mock data
-import products from '../products';
+import React, { useState, useEffect } from 'react';
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // Destructing the res object to get data instead of writing res.data
+      const { data } = await axios.get('api/products');
+      setProducts(data);
+
+      // try {
+      //   // Destructing the res object to get data instead of writing res.data
+      //   const { data } = await axios.get('api/products');
+      //   setProducts(data);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Row>
