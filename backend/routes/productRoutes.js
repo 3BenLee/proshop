348cee89120e33cfd797eaa66742b774818b1e16
BenLee3 <bendesigns76@gmail.com>
2022-04-04 23:12:03 +0900

    Setup product route

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
+
+const router = express.Router();
+
+// @desc   Fetch all Products
+// @route  Get api/products
+// @access Public
+router.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @desc   Fetch all Products
+// @route  Get api/products/:id
+// @access Public
+router.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product Not Found' });
+    }
+  })
+);
+
+export default router;
